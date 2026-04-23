7f003dd31f0fd0f99a4ca7b9cbf2784b45e8049c
anacarmona10 <anacarmona1023@gmail.com>
2026-04-23 14:36:36 -0500

    Estructura inicial del taller

---
+++ src/Main.kt
@@ -0,0 +1,10 @@
+import ejercicios.*
+
+fun main() {
+    println("INICIANDO TALLER DE KOTLIN")
+
+    ejecutarBloque1()
+
+
+    println("FIN DEL TALLER")
+}
---
+++ src/ejercicios/Bloque1Variables.kt
@@ -0,0 +1,8 @@
+package ejercicios
+
+fun ejecutarBloque1() {
+    println(" BLOQUE 1: VARIABLES ")
+
+    val nombre = "Juan"
+    println("1. Nombre: $nombre")
+}
